7d44ddbca7731ff8151e4b3b4e43354b7882893a
Hamza <hola@gmail.com>
2018-09-19 18:34:07 +0200

    Afegit nou arxiu amb noves instruccions

---
+++ suma.java
@@ -0,0 +1,10 @@
+1.
+2. int numero1 = 0;
+3. int numero2 = 0;
+4.
+5. System.out.println("Introduce el primer número:");			
+6. numero1 = reader.nextInt();
+7. 
+8. System.out.println("Introduce el segundo número:");
+9. numero2 = reader.nextInt() 
+10.
