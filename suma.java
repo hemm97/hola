9fff9eca232b80fa5b1b2069bede5bba9c0ed735
Hamza <hola@gmail.com>
2018-09-19 18:56:48 +0200

    Lidioma esta en catala

--- suma.java
+++ suma.java
@@ -2,9 +2,9 @@
 2. int numero1 = 0;
 3. int numero2 = 0;
 4.
-5. System.out.println("Enter the first number:");			
+5. System.out.println("Escriu el primer numero:");			
 6. numero1 = reader.nextInt();
 7. 
-8. System.out.println("Enter the second number:");
+8. System.out.println("Escribe el segundo numero:");
 9. numero2 = reader.nextInt() 
 10.
