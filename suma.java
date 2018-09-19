35a4d3451f657e68eb629dd7bb7b68296c368961
Hamza <hola@gmail.com>
2018-09-19 19:11:09 +0200

    El fitxer esta amb castella

--- suma.java
+++ suma.java
@@ -2,7 +2,7 @@
 2. int numero1 = 0;
 3. int numero2 = 0;
 4.
-5. System.out.println("Escriu el primer numero:");			
+5. System.out.println("Escrive el primer numero:");			
 6. numero1 = reader.nextInt();
 7. 
 8. System.out.println("Escribe el segundo numero:");
