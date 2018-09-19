ebb3c745f8cae4559aff3881d3dedc3ffff3d9e5
Hamza <hola@gmail.com>
2018-09-19 18:46:01 +0200

    S'ha modificat l'idioma amb angles

--- suma.java
+++ suma.java
@@ -2,9 +2,9 @@
 2. int numero1 = 0;
 3. int numero2 = 0;
 4.
-5. System.out.println("Introduce el primer número:");			
+5. System.out.println("Enter the first number:");			
 6. numero1 = reader.nextInt();
 7. 
-8. System.out.println("Introduce el segundo número:");
+8. System.out.println("Enter the second number:");
 9. numero2 = reader.nextInt() 
 10.
