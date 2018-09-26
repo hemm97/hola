6352d0abe2d8d35108c2c8fd35e0214e6fd97a81
hemm97 <43611465+hemm97@users.noreply.github.com>
2018-09-26 16:24:23 +0200

    Update suma.java

--- suma.java
+++ suma.java
@@ -1,7 +1,11 @@
 int numero1 = 0;
 int numero2 = 0;
+int numero3 = 0;
 System.out.println("Enter first number");			
 numero1 = reader.nextInt();
- 
+
+System.out.println("Escriu un segon numero");			
+numero3 = reader.nextInt();
+
 System.out.println("Enter the second number:");
 numero2 = reader.nextInt() 
